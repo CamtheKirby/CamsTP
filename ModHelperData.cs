8bc1abaa492b26b34a940187fc9f0932778147c1
Cam the Kirby <92703353+CamtheKirby@users.noreply.github.com>
2023-11-08 15:03:46 -0600

    Last Update

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,7 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string Name = "Cams Tower Pack!";
 
-    public const string Description = "All My Towers In a Pack!";
+    public const string Description = "DOWNLOAD CAM'S PACK INSTEAD OF THIS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! All My Towers In a Pack!";
 
     public const string RepoOwner = "CamtheKirby"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "CamsTP"; // TODO add your repo name here, also in the download url in README.md
